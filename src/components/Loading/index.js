25867202b42660d72b9a3a8988e6ee85dcb69fbf
T-Brave <38653760+T-Brave@users.noreply.github.com>
2020-04-07 15:37:42 +0800

    loading加载组件

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,1 +1,19 @@
+import React from "react";
+import { Spin, Alert } from "antd";
 
+class Loading extends React.Component{
+    render() {
+        return(
+             <div className="example">
+                <Spin tip="Loading..." size="large">
+                    <Alert message="Alert message title"
+                        description="Further details about the context of this alert."
+                        type="info">
+                    </Alert>
+                </Spin>
+            </div>
+        )
+    }
+}
+
+export default  Loading;
