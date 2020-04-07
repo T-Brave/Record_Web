f22d15d30bd1c9b0126aad08eedb27bf8ec5ba92
T-Brave <38653760+T-Brave@users.noreply.github.com>
2020-04-07 15:45:47 +0800

    menus文件

---
+++ src/router/menus.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        title:"录音文件",
+        icon:"home",
+        key:"/recording"
+    },
+    {
+        title:"用户管理",
+        icon:"home",
+        key:"/user"
+    },
+    {
+        title:"高级配置",
+        icon:"home",
+        key:"/advance"
+    },
+]
