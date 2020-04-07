120ffe62e21b493e01b8d3a5782cff4d0a3da3bd
T-Brave <38653760+T-Brave@users.noreply.github.com>
2020-04-07 15:46:11 +0800

    router文件

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,1 +1,29 @@
+/* Loadable插件使用Loading */
+import Loadable from "react-loadable";
+import Loading from "../components/Loading";
 
+//定义路由
+
+global.Recording = Loadable({
+    loader:()=>
+        import('../views/pages/recording'),
+    loading:Loading
+});
+
+global.Advance = Loadable({
+    loader:()=>
+        import('../views/pages/advance'),
+    loading:Loading
+});
+
+global.User = Loadable({
+    loader:()=>
+        import('../views/pages/user'),
+    loading:Loading
+});
+
+global.Error_404 = Loadable({
+    loader: () =>
+        import('../views/pages/error_404'),
+    loading: Loading,
+});
