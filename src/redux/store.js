3f6592d3ee260f833c4fec20de450c816d7f5c70
T-Brave <38653760+T-Brave@users.noreply.github.com>
2020-04-07 15:43:09 +0800

    store文件

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,23 @@
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension"; //redux调试工具
 
+import infoReducer from './reducers/info';
+import UserLoginReducer from "./reducers/UserLogin";
+
+const reducer = combineReducers({
+    info: infoReducer,
+    userlogin: UserLoginReducer
+});
+
+const store = createStore(reducer, {});
+
+store.subscribe(() => {
+    let state = store.getState();
+});
+
+// store.dispatch({
+//     type: "SET_NAME",
+//     name: "开心超人"
+// });
+
+export default store;
